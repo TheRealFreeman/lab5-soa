1707bdd62ace9f39e95235824b19930ad729a9fd
Enrique Ruiz <766685@celes.unizar.es>
2020-12-08 16:45:12 +0100

    Removed deprecated method and commented code

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -11,13 +11,17 @@ public class Router extends RouteBuilder {
   @Override
   public void configure() {
     from(DIRECT_URI).process(exchange -> {
+      // Get current body
       String body = exchange.getIn().getBody(String.class);
+      // Splits string by the operator "max:""
       String[] splitBody = body.split("max:");
       String newBody = splitBody[0];
       if (splitBody.length > 1) {
+        // Limit number of tweets to the chosen amount
         newBody += "?count=" + splitBody[1];
       }
-      exchange.getOut().setBody(newBody);
+      // Sets updated body
+      exchange.getIn().setBody(newBody);
     }).log("Body contains \"${body}\"").log("Searching twitter for \"${body}\"!").toD("twitter-search:${body}")
         .log("Body now contains the response from twitter:\n${body}");
   }
